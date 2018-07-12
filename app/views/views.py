1ca871fc115f24cba3e174f2926a90b871cbd160
SelaDanti <kwameasiago@gmail.com>
2018-07-12 22:52:17 +0300

    [:pray: Feature #158949454] Move config to enviroment

--- app/views/views.py
+++ app/views/views.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restplus import Api
 from werkzeug.contrib.fixers import ProxyFix
@@ -5,7 +7,6 @@ from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
 app.config.from_envvar('APP_SETTINGS')
-app.config['APP_SETTINGS'] = '../../config.cfg'
 app.wsgi_app = ProxyFix(app.wsgi_app)
 api = Api(app,version=2.0,title='Ride My Way')
 

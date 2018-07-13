7492663cf97c60840887628845217ef141342494
SelaDanti <kwameasiago@gmail.com>
2018-07-13 23:08:34 +0300

    :twisted_rightwards_arrows: [Featur #159037594] Refactor test

--- test/clean.py
+++ test/clean.py
@@ -3,8 +3,6 @@ from app.model.con import con
 
 def delete_record(email):
 	cur = con.cursor()
-	sql = """
-	DELETE FROM users WHERE EMAIL = '{}'
-	""".format(email)
+	sql = "DELETE FROM users;"
 	cur.execute(sql)
 	con.commit()

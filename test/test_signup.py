eef2520c2c35e9ebb92b5947823a9a3215e55af2
SelaDanti <kwameasiago@gmail.com>
2018-07-13 22:46:28 +0300

    :twisted_rightwards_arrows: [Featur #159037594] Refactor test

--- test/test_signup.py
+++ test/test_signup.py
@@ -20,37 +20,33 @@ class  UserTest(unittest.TestCase):
 		}
 		self.content_type = 'application/json'
 		self.test = app.test_client()
-		self.delete_record = delete_record(self.user_data['email'])
+		app.config['DBNAME'] = 'test_ridemyway'
 
 	def tearDown(self):
+		delete_record(self.user_data['email'])
 		self.user_data = None
 		self.content_type = None
 		self.test = None
-		self.delete_record = None
 
 	def test_signup_status_code(self):
 		response = self.test.post('/auth/signup', data=json.dumps(self.user_data),content_type=self.content_type)
 		self.assertEqual(201,response.status_code)
-		self.delete_record
 
 	def test_signup_output(self):
 		response = self.test.post('/auth/signup', data=json.dumps(self.user_data),content_type=self.content_type)
 		data = json.loads(response.get_data().decode('UTF-8'))
 		self.assertEqual('Account created',data['result'])
-		self.delete_record
 
 	def test_email_exist_status_code(self):
 		response = self.test.post('/auth/signup', data=json.dumps(self.user_data),content_type=self.content_type)
 		response = self.test.post('/auth/signup', data=json.dumps(self.user_data),content_type=self.content_type)
 		self.assertEqual(405,response.status_code)
-		self.delete_record
 
 	def test_email_exist_output(self):
 		response = self.test.post('/auth/signup', data=json.dumps(self.user_data),content_type=self.content_type)
 		response = self.test.post('/auth/signup', data=json.dumps(self.user_data),content_type=self.content_type)
 		data = json.loads(response.get_data().decode('UTF-8'))
 		self.assertEqual('Email exist',data['result'])
-		self.delete_record
 
 
 if __name__ == '__main__':

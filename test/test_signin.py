2da372f98081e04cd25fb4e75a9b0414fd0374c7
SelaDanti <kwameasiago@gmail.com>
2018-07-13 23:27:06 +0300

    :twisted_rightwards_arrows: [Featur #159037594] Refactor test

--- test/test_signin.py
+++ test/test_signin.py
@@ -33,6 +33,7 @@ class TestSignIn(unittest.TestCase):
 		self.test = app.test_client()
 
 	def tearDown(self):
+		delete_record()
 		self.wrong_email = None
 		self.wrong_password = None
 		self.signup = None
@@ -57,7 +58,6 @@ class TestSignIn(unittest.TestCase):
 		response = self.test.post('auth/login',content_type=self.content_type,
 			data=json.dumps(self.wrong_password))
 		self.assertEqual(response.status_code,405)
-		delete_record()
 
 	def test_invalid_password_output(self):
 		response = self.test.post('auth/signup',content_type=self.content_type,
@@ -66,7 +66,6 @@ class TestSignIn(unittest.TestCase):
 			data=json.dumps(self.wrong_password))
 		data = json.loads(response.get_data().decode("UTF-8"))
 		self.assertEqual(data['result'],'Invalid password')
-		delete_record()
 
 	def test_valid_data_status_code(self):
 		response = self.test.post('auth/signup',content_type=self.content_type,
@@ -74,7 +73,6 @@ class TestSignIn(unittest.TestCase):
 		response = self.test.post('auth/login',content_type=self.content_type,
 			data=json.dumps(self.valid_data))
 		self.assertEqual(response.status_code,200)
-		delete_record()
 
 	def test_valid_data_output(self):
 		response1 = self.test.post('auth/signup',content_type=self.content_type,
@@ -83,7 +81,6 @@ class TestSignIn(unittest.TestCase):
 			data=json.dumps(self.valid_data))
 		data = json.loads(response.get_data().decode("UTF-8"))
 		self.assertIn('Token',str(data))
-		delete_record()
 
 if __name__ == '__main__':
 	unittest.main()
